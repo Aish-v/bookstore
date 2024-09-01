c68457640abd68809dfe8b901cdb81b797780d2c
Aishwarya <aish08003@gmail.com>
2024-09-01 16:07:28 +0530

    .gitignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 import CreateBooks from './pages/CreateBooks';
 import ShowBook from './pages/ShowBook';
 import EditBook from './pages/EditBook';
-import DeleteBooks from './pages/DeleteBooks';
+import DeleteBook from './pages/DeleteBook';
 const App=()=>{
   return(
     <Routes>
@@ -12,7 +12,7 @@ const App=()=>{
       <Route path='/books/create' element={<CreateBooks/>}/>
       <Route path='/books/details/:id' element={<ShowBook/>}/>
       <Route path='/books/edit/:id' element={<EditBook/>}/>
-      <Route path='/books/delete/:id' element={<DeleteBooks/>}/>
+      <Route path='/books/delete/:id' element={<DeleteBook/>}/>
     </Routes>
 
   );
